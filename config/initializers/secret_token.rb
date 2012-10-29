50f81a88202fe438bd0c7467109610dcf5f51ff9
akiii <akiii@sfc.wide.ad.jp>
2012-10-29 12:44:13 +0900

    Setup a project.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Meshi365::Application.config.secret_token = '0195d7eeaac2efe6f039f379dafff60d27e097a4a93e2c343f1f36453f793ac06f7d989b8fdb188e851fce653c74aa7fff4310fc61413168cc5383d85008f9b2'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Meshi365::Application.config.session_store :cookie_store, key: '_meshi365_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Meshi365::Application.config.session_store :active_record_store
