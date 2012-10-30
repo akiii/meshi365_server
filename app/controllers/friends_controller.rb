00d020e2f094b48542454f35ef4829efd3878011
akiii <akiii@sfc.wide.ad.jp>
2012-10-31 08:28:29 +0900

    Edited index and Added send friend request method.

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -1,8 +1,25 @@
 class FriendsController < ApplicationController
 
   def index
-    friends = Friend.find_all_by_my_id(params[:id])
-    render :json => friends
+    user = User.find_by_uiid(params[:uiid])
+    render :json => user.friends
+  end
+
+  def send_friend_request
+    me = User.find_by_uiid(params[:my_uiid])
+    request_user = User.find_by_uiid(params[:to_user_uiid])
+    if me != nil && request_user != nil && params[:my_uiid] != params[:to_user_uiid]
+      req = FriendRequest.find_by_my_id_and_from_user_id(me.id, request_user.id)
+      if req
+        FriendRequest.delete(req.id)
+        Friendship.create(:user_id => me.id, :friend_id => request_user.id)
+        Friendship.create(:user_id => request_user.id, :friend_id => me.id)
+      else
+        unless FriendRequest.exists?(:my_id => request_user.id, :from_user_id => me.id)
+          FriendRequest.create(:my_id => request_user.id, :from_user_id => me.id)
+        end
+      end
+    end
   end
 
 end
