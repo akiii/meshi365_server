7b2f3e97c0cb1142f225daeb93537f514ca2b0d4
akiii <akiii@sfc.wide.ad.jp>
2012-11-01 00:28:27 +0900

    Modified columns.

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -7,17 +7,18 @@ class FriendsController < ApplicationController
 
   def send_friend_request
     me = User.find_by_uiid(params[:my_uiid])
-    request_user = User.find_by_uiid(params[:to_user_uiid])
-    if me != nil && request_user != nil && params[:my_uiid] != params[:to_user_uiid]
-      req = FriendRequest.find_by_user_id_and_requested_user_id(me.id, request_user.id)
-      if req
-        FriendRequest.delete(req.id)
-        Friendship.create(:user_id => me.id, :friend_id => request_user.id)
-        Friendship.create(:user_id => request_user.id, :friend_id => me.id)
-      else
-        unless FriendRequest.exists?(:user_id => request_user.id, :requested_user_id => me.id)
-          FriendRequest.create(:user_id => request_user.id, :requested_user_id => me.id)
-        end
+    requested_user = User.find_by_uiid(params[:to_user_uiid])
+    if me != nil && requested_user != nil && params[:my_uiid] != params[:to_user_uiid]
+      req = FriendRequest.find_by_user_id_and_requested_user_id(me.id, requested_user.id)
+      unless req
+        requested = FriendRequest.find_by_user_id_and_requested_user_id(requested_user.id, me.id)
+        if requested
+          FriendRequest.delete(requested.id)
+          Friendship.create(:user_id => me.id, :friend_id => requested_user.id)
+          Friendship.create(:user_id => requested_user.id, :friend_id => me.id)
+        else
+          FriendRequest.create(:user_id => me.id, :requested_user_id => requested_user.id)
+	end
       end
     end
   end
