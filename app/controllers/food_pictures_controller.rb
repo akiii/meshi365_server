c03ca96d4d30a6a0e67747cf32f6d707a0471b03
akiii <akiii@sfc.wide.ad.jp>
2012-11-01 10:05:56 +0900

    Modified post food picture method.

--- app/controllers/food_pictures_controller.rb
+++ app/controllers/food_pictures_controller.rb
@@ -7,10 +7,12 @@ class FoodPicturesController < ApplicationController
 
   def post
     picture = FoodPicture.new
-    picture.user_id    = params[:user_id]
+    picture.user_id    = User.find_by_uiid(params[:uiid]).id
+    picture.type       = params[:type]
     picture.url        = params[:url]
     picture.store_name = params[:store_name]
     picture.menu_name  = params[:menu_name]
+    picture.comment    = params[:comment]
     picture.star_num   = params[:star_num]
     picture.save
   end
