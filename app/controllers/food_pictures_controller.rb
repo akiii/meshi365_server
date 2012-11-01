5143942b16b59579b09f2e6c0a45fbb5ecd15bf8
akiii <akiii@sfc.wide.ad.jp>
2012-11-01 15:58:08 +0900

    Did comment outs.

--- app/controllers/food_pictures_controller.rb
+++ app/controllers/food_pictures_controller.rb
@@ -8,19 +8,18 @@ class FoodPicturesController < ApplicationController
   def post
     picture = FoodPicture.new
     picture.user_id    = User.find_by_uiid(params[:uiid]).id
-    picture.type       = params[:meal_type]
+#    picture.type       = params[:meal_type]
     picture.url        = params[:url]
-    if params[:store_name]
-      picture.store_name = params[:store_name]
-    end
-    if params[:menu_name]
-      picture.menu_name  = params[:menu_name]
-    end
-    if params[:comment]
-      picture.comment    = params[:comment]
-    end
-    picture.star_num   = params[:star_num]
-    picture.save
+#    if params[:store_name]
+#      picture.store_name = params[:store_name]
+#    end
+#    if params[:menu_name]
+#      picture.menu_name  = params[:menu_name]
+#    end
+#    if params[:comment]
+#      picture.comment    = params[:comment]
+#    end
+#    picture.star_num   = params[:star_num]
     if picture.save
       render :json => { "save" => true }
     else
