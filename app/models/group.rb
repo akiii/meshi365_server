ed0866e58c7df6ea043bc789d6585ea3d28247dc
akiii <akiii@sfc.wide.ad.jp>
2012-11-03 08:20:37 +0900

    Added relationships.

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,3 +1,6 @@
 class Group < ActiveRecord::Base
   attr_accessible :amenity, :max_parcentage, :min_parcentage
+
+  has_many :group_relationships, :dependent => :destroy
+  has_many :users, :through => :group_relationships
 end
